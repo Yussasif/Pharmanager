4d09796f8d4d9cd382f128749a0a3d2f9cf67843
cas1908 <schima05@gmail.com>
2023-04-05 05:34:04 +0100

    tailwind installed

---
+++ pharmanager/pharmanager/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./inndex.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
