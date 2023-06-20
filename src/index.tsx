503bdb8d825234c2f3b148b8534b7c39214e73cc
Nnaemeka <emekason128@gmail.com>
2023-06-20 10:45:48 +0100

    table component created

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { SidebarContextProvider } from "./store/sidebarContext";
 
-
 ReactDOM.render(
   <SidebarContextProvider>
     <App />
