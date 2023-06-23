e9cd785b41aebbd7a9bf4d0bb8f9ae7fbee02d21
Prom1234pro <promiseimadonmwinyi@gmail.com>
2023-06-23 18:44:14 -0400

    updating main branch

--- src/components/tables/PrescriptionTable/PrescriptionTable.tsx
+++ src/components/tables/PrescriptionTable/PrescriptionTable.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, Key } from "react";
+import { useState, useEffect } from "react";
 import styles from './tables.module.scss';
 import { InlineIcon } from '@iconify/react';
 import filterPrescriptions from "./utils/filterPrescriptions";
